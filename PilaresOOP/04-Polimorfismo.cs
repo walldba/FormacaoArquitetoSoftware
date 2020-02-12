e46e6edd322f34f383261c7531498b0a504bf0dc
Wallace Willer Jeronimo <wallace.jeronimo@aec.com.br>
2020-02-12 17:03:01 -0200

    Criacao de classe polimorfismo

---
+++ PilaresOOP/04-Polimorfismo.cs
@@ -0,0 +1,29 @@
+namespace PilaresOOP
+{
+    public class CafeteiraEspressa : Eletrodomestico
+    {
+        public CafeteiraEspressa(string nome, int voltagem)
+        : base(nome, voltagem) { }
+
+        public CafeteiraEspressa()
+       : base("Padrao", 220) { }
+
+        private static void AquecerAgua() { }
+        private static void MoerGraos() { }
+        private static void PrepararCafe()
+        {
+            AquecerAgua();
+            MoerGraos();
+        }
+
+        public override void Ligar()
+        {
+
+        }
+
+        public override void Ligar()
+        {
+
+        }
+    }
+}
