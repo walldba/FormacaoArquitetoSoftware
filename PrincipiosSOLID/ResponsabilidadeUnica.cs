14cf5feeb8b78b359128c6c9052efb25a57090cf
Wallace Willer Jeronimo <wallace.jeronimo@aec.com.br>
2020-03-02 19:49:19 -0300

    Adicionando classe de demonstracao para ResponsabilidadeUnica

---
+++ PrincipiosSOLID/ResponsabilidadeUnica.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PrincipiosSOLID
+{
+    public class ResponsabilidadeUnica
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+        public bool Validar()
+        {
+            //O Cliente deve ser capaz de validar seus dados, como CPF, Email...
+        }
+    }
+}
