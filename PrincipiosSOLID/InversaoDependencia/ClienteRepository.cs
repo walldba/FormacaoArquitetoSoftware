8e866a0cc27f25c32857a3974dd6621aba2a11c5
Wallace Willer Jeronimo <wallace.jeronimo@aec.com.br>
2020-03-11 19:40:31 -0300

    Criacao de cliente repository

---
+++ PrincipiosSOLID/InversaoDependencia/ClienteRepository.cs
@@ -0,0 +1,13 @@
+using InversaoDependencia.Interfaces;
+using InversaoDependencia.Models;
+
+namespace InversaoDependencia
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        public void AdicionarCliente(Cliente cliente)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
