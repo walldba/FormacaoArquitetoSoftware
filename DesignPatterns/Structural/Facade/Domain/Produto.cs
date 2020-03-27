00154f220bf38171e7ee57d7e808f6d15e76e25d
wallace.jeronimo <linkin01>
2020-03-27 18:53:44 -0300

    Implementacao de exemplo Facade Pattern

---
+++ DesignPatterns/Structural/Facade/Domain/Produto.cs
@@ -0,0 +1,8 @@
+﻿namespace DesignPatterns.Structural.Facade.Domain
+{
+    public class Produto    
+    {
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
---
+++ DesignPatterns/Structural/Facade/ExecucaoFacade.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using DesignPatterns.Structural.Facade.CrossCutting;
+using DesignPatterns.Structural.Facade.Domain;
+
+namespace DesignPatterns
+{
+    public class ExecucaoFacade
+    {
+        public static void Executar()
+        {
+            var produtos = new List<Produto>
+            {
+                new Produto{Nome = "Tenis Adidas", Valor = new Random().Next(500)},
+                new Produto{Nome = "Camisa Boliche", Valor = new Random().Next(500)},
+                new Produto{Nome = "Raquete Tenis", Valor = new Random().Next(500)}
+            };
+
+            var pedido = new Pedido
+            {
+                Id = Guid.NewGuid(),
+                Produtos = produtos
+            };
+
+            var pagamento = new Pagamento
+            {
+                CartaoCredito = "5555 2222 5555 9999"
+            };
+
+            // Resolver com DI
+            var pagamentoService = new PagamentoCartaoCreditoService(new PagamentoCartaoCreditoFacade(new PayPalGateway(), new ConfigurationManager()));
+
+            var pagamentoResult = pagamentoService.RealizarPagamento(pedido, pagamento);
+
+            Console.WriteLine(pagamentoResult.Status);
+        }
+    }
+
+}
